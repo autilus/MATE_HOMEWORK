f5617c27b66ac6540b06bdeaad24c648b4fc849b
autilus <smasim2016@gmail.com>
2019-06-04 00:14:21 +0300

    homework 04.06)

--- src/main/java/com/autilus/hw0406/task2/App.java
+++ src/main/java/com/autilus/hw0406/task2/App.java
@@ -5,6 +5,6 @@ import java.io.NotSerializableException;
 public class App {
     public static void main(String[] args) throws CloneNotSupportedException, NotSerializableException {
         Car bmw = new Car("BMW", "x5", 2018, 280);
-        Car bmwClone = (Car) Car.deepClone(bmw);
+        Car bmwClone = Car.deepClone(bmw);
     }
 }
