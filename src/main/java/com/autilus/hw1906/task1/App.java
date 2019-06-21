904ed4d46a57327aa6ed179048f6140934ef4558
autilus <smasim2016@gmail.com>
2019-06-21 10:01:09 +0300

    fixed

--- src/main/java/com/autilus/hw1906/task1/App.java
+++ src/main/java/com/autilus/hw1906/task1/App.java
@@ -4,9 +4,6 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class App {
-    public static void main(String[] args) {
-    }
-
     public static <T, U> Function<T, U> ternaryOperator(
             Predicate<? super T> condition,
             Function<? super T, ? extends U> ifTrue,
--- src/main/java/com/autilus/hw1906/task2/App.java
+++ src/main/java/com/autilus/hw1906/task2/App.java
@@ -3,9 +3,6 @@ package com.autilus.hw1906.task2;
 import java.util.function.DoubleUnaryOperator;
 
 public class App {
-    public static void main(String[] args) {
-    }
-
     public static double integrate(DoubleUnaryOperator f, double a, double b) {
         double x = 0;
         double h = 1.E-6;
