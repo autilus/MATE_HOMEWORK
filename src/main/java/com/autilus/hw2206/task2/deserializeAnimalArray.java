22e8a93429fdc8afb1c70a55dc0fe7f27ebee378
autilus <smasim2016@gmail.com>
2019-06-23 22:32:24 +0300

    home work 22/06

---
+++ src/main/java/com/autilus/hw2206/task2/deserializeAnimalArray.java
@@ -0,0 +1,23 @@
+package com.autilus.hw2206.task2;
+
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+
+public class deserializeAnimalArray {
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        int count = 0;
+        Animal[] animals = new Animal[0];
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data);
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
+            count = objectInputStream.readInt();
+            animals = new Animal[count];
+            for (int i = 0; i < count; i++) {
+                Animal animal = (Animal) objectInputStream.readObject();
+                animals[i] = animal;
+            }
+        } catch (Exception e) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        return animals;
+    }
+}
