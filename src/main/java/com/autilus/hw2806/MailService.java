a24a61d3981289e7fd7b2d71481717d057195844
autilus <smasim2016@gmail.com>
2019-07-02 18:24:30 +0300

    fix

--- src/main/java/com/autilus/hw2806/MailService.java
+++ src/main/java/com/autilus/hw2806/MailService.java
@@ -1,7 +1,7 @@
 package com.autilus.hw2806;
 
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -15,8 +15,13 @@ public static class MailService<T> implements Consumer<Send<T>> {
     }
 
     public MailService() {
-        box = (HashMap) get(key) ->{ return super.getOrDefault(key, new LinkedList<T>()); };
-
+        box = new HashMap<String, List<T>>() {
+            @Override
+            public List<T> get(Object key) {
+                return getOrDefault(key, Collections.emptyList());
+//                return getOrDefault(key, new LinkedList<T>());
+            }
+        };
     }
 
     public Map<String, List<T>> getMailBox() {
